71d7418d8a80fe930a9b2689ee3e62468c0872d9
Jesus Fernandez <jesus.fndz@gmail.com>
2014-05-08 00:10:44 -0400

    Fix a typo in the occupations migration file name
    Signed-off-by: Jesus Fernandez <jesus.fndz@gmail.com>

--- app/database/migrations/2014_04_22_220557_create_occupations_table.php
+++ app/database/migrations/2014_04_22_220557_create_occupations_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOccupationTable extends Migration {
+class CreateOccupationsTable extends Migration {
 
 	/**
 	 * Run the migrations.
