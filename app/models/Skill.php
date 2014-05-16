8d77def8264512a0872155a02c0cf7e1c2559d6d
Jesus Fernandez <jesus.fndz@gmail.com>
2014-05-15 22:25:21 -0400

    Change spaces to tabs
    Signed-off-by: Jesus Fernandez <jesus.fndz@gmail.com>

--- app/models/Skill.php
+++ app/models/Skill.php
@@ -1,18 +1,19 @@
 <?php
 
-class Skill extends BaseModel {
+class Skill extends BaseModel
+{
 
-  /**
-   * The database table used by the model.
-   *
-   * @var string
-   */
-  protected $table = 'skills';
-  protected $guarded = ['id'];
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'skills';
+	protected $guarded = ['id'];
 
-  public function skillSets()
-  {
-    return $this->hasMany('SkillSet');
-  }
+	public function skillSets()
+	{
+		return $this->hasMany('SkillSet');
+	}
 
 }
--- app/models/SkillSet.php
+++ app/models/SkillSet.php
@@ -1,28 +1,38 @@
 <?php
 
-class SkillSet extends BaseModel {
+class SkillSet extends BaseModel
+{
 
-  /**
-   * The database table used by the model.
-   *
-   * @var string
-   */
-  protected $table = 'skill_sets';
-  protected $guarded = ['id'];
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'skill_sets';
+	protected $guarded = ['id'];
+	protected static $rules = ['name' => 'required'];
 
-  public function users()
-  {
-    return $this->belongsTo('User');
-  }
+	public function users()
+	{
+		return $this->belongsTo('User');
+	}
 
-  public function skill()
-  {
-    return $this->belongsTo('Skill');
-  }
+	public function skill()
+	{
+		return $this->belongsTo('Skill');
+	}
 
-  public function tags()
-  {
-    return $this->belongsToMany('Tag');
-  }
+	public function tags()
+	{
+		return $this->belongsToMany('Tag');
+	}
+
+	/**
+	 * @return array
+	 */
+	public static function getValidations()
+	{
+		return self::$rules;
+	}
 
 }
