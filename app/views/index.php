61be481bb88e0d93afaca8fa0713367f04efdd2f
Jesus Fernandez <jesus.fndz@gmail.com>
2014-05-24 10:38:40 -0400

    Link the sample landing page to the projects sample page
    Signed-off-by: Jesus Fernandez <jesus.fndz@gmail.com>

--- app/views/index.php
+++ app/views/index.php
@@ -44,7 +44,7 @@
 			<ul class="nav navbar-nav navbar-right">
 				<li class="active"><a href="/">Home</a></li>
 				<li><a href="#">Talents</a></li>
-				<li><a href="#">Projects</a></li>
+				<li><a href="/projects">Projects</a></li>
 				<li><a href="#about">About</a></li>
 				<li><a href="#contact">FAQ</a></li>
 				<li><a href="#">Login</a></li>
