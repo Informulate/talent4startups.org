cbdebbd79bbc21139883021891314aa393f33d49
Jesus Fernandez <jesus.fndz@gmail.com>
2014-09-07 12:28:47 -0400

    Display a dashboard when user is logged on
    Fixes #99
    
    Signed-off-by: Jesus Fernandez <jesus.fndz@gmail.com>

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -4,6 +4,10 @@ class PagesController extends \BaseController {
 
 	public function home()
 	{
+		if (Auth::check()) {
+			return View::make('pages.dashboard');
+		}
+
 		return View::make('pages.home');
 	}
 }
---
+++ app/views/pages/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+
+@section('content')
+	<div class="row">
+		<div class="col-sm-12">
+			<h1>This will be the logged on user's dashboard.</h1>
+		</div>
+	</div>
+@stop
