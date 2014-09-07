71c5ad1dcdc3289637c70b65b1e9259fcabec6d9
Jesus Fernandez <jesus.fndz@gmail.com>
2014-09-07 19:20:31 -0400

    Add initial list of talents
    * Still missing most properties
    
    Signed-off-by: Jesus Fernandez <jesus.fndz@gmail.com>

---
+++ app/views/talent/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.default')
+
+@section('content')
+	<div class="row">
+		@foreach($talents as $talent)
+			<div class="col-sm-6 col-md-3">
+				<div class="thumbnail">
+					<img data-src="holder.js/300x300" alt="...">
+					<div class="caption">
+						<h3><?php echo $talent->email; ?></h3>
+						<h6><i class="glyphicons google_maps"></i>Orlando, FL.</h6>
+						<p>Project Needs: Developers, Writers, Project Managers</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit dolorem eius, asperiores magnam perspiciatis dolor ratione dolores impedit qui. Rerum amet, iusto.  Eaque neque expedita similique veniam nihil ab perspiciatis.</p>
+						<p><a href="#" class="btn btn-primary pull-right" role="button">Learn More</a></p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		@endforeach()
+	</div>
+@stop
