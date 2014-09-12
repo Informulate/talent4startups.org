1faf58d9ff959ecbd046661a2675ca02edbb49ba
Jesus Fernandez <jesus.fndz@gmail.com>
2014-09-12 19:22:55 -0400

    Fix projects layout issues
    Signed-off-by: Jesus Fernandez <jesus.fndz@gmail.com>

--- app/views/project/index.blade.php
+++ app/views/project/index.blade.php
@@ -9,14 +9,14 @@
 	<div class="row">
 		@foreach($projects as $project)
 			<div class="col-sm-6 col-md-3">
-				<div class="thumbnail">
+				<div class="thumbnail projects">
 					<img data-src="holder.js/300x300" alt="...">
 					<div class="caption">
 						<h3><?php echo $project->name; ?></h3>
 						<h6><i class="glyphicons google_maps"></i>Orlando, FL.</h6>
 						<p>Project Needs: Developers, Writers, Project Managers</p>
 						<p>{{ Str::limit( $project->description, 50 ) }}</p>
-						<p><a href="{{ route('projects.show', $project->url) }}" class="btn btn-primary pull-right" role="button">Learn More</a></p>
+						<p><a href="{{ route('projects.show', $project->url) }}" class="btn btn-primary pull-right learn-more" role="button">Learn More</a></p>
 					</div>
 					<div class="clearfix"></div>
 				</div>
