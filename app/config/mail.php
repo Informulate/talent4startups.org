ec283dd0bfbd8acae8e8c3a1c7ddfd3621a83bb1
Jesus Fernandez <jesus.fndz@gmail.com>
2014-10-31 12:41:20 -0400

    Mail configuration changes
    * Use mailgun for sending emails
    * Move mail configuration to environment variables
    
    Re #21
    
    Signed-off-by: Jesus Fernandez <jesus.fndz@gmail.com>

--- app/config/mail.php
+++ app/config/mail.php
@@ -15,7 +15,7 @@ return array(
 	|
 	*/
 
-	'driver' => 'smtp',
+	'driver' => 'mailgun',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => getenv('EMAIL_ADDRESS'), 'name' => getenv('EMAIL_NAME')),
 
 	/*
 	|--------------------------------------------------------------------------
--- app/config/services.php
+++ app/config/services.php
@@ -15,8 +15,8 @@ return array(
 	*/
 
 	'mailgun' => array(
-		'domain' => '',
-		'secret' => '',
+		'domain' => getenv('MAILGUN_DOMAIN'),
+		'secret' => getenv('MAILGUN_SECRET'),
 	),
 
 	'mandrill' => array(
