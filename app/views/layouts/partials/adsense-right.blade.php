2bbafc46833f2b9b4eb97797dde82ff96398b8d1
Frank Bennett <frank.bennett@informulate.net>
2015-01-30 19:28:34 -0500

    new AdSense layout

--- app/views/layouts/partials/adsense-right.blade.php
+++ app/views/layouts/partials/adsense-right.blade.php
@@ -1,6 +1,6 @@
 <div id="adsense">
     <img class="center-block img-responsive" src="https://storage.googleapis.com/support-kms-prod/SNP_586A5ACE683FE8BE05136C11100EE28C4316_3285962_en_v0">
-    <img class="center-block img-responsive" src="https://static.adzerk.net/Advertisers/d936d243e9de4c989a6c95b031eb11d6.png">
     <img class="center-block img-responsive" src="https://storage.googleapis.com/support-kms-prod/SNP_2922332_en_v0">
-    <img class="center-block img-responsive" src="https://static.adzerk.net/Advertisers/d936d243e9de4c989a6c95b031eb11d6.png">
+    <img class="center-block img-responsive" src="https://storage.googleapis.com/support-kms-prod/SNP_2922332_en_v0">
+    <img class="center-block img-responsive" src="https://storage.googleapis.com/support-kms-prod/SNP_2922332_en_v0">
 </div>
