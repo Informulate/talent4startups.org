d6a7eecf027e6e40962cd570d41ae11ea8495c8c
Mike Bernat <mike.bernat@informulate.net>
2015-02-10 15:12:53 +0000

    Ticket #124 Registration emails not working

--- app/views/startups/create.blade.php
+++ app/views/startups/create.blade.php
@@ -50,7 +50,7 @@
                 });
 
                 $(formClone).find('.remove').on('click', function() {
-                    $(this).parent('.need').remove();
+                    $(this).closest('.need').remove();
                 });
                 cloneIndex++;
 
@@ -73,7 +73,7 @@
             });
 
             $('.need .remove').on('click', function() {
-                $(this).parent('.need').remove();
+                $(this).closest('.need').remove();
             });
             $('.startup-needs .need .tags').select2({
                 'tags': [
