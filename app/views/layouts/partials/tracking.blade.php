09d67f80c7e4fd62c9e2497c1b8749efaeebe98d
Mike Bernat <mike.bernat@informulate.net>
2015-02-11 19:42:25 +0000

    Added hotjar tracking code

---
+++ app/views/layouts/partials/tracking.blade.php
@@ -0,0 +1,10 @@
+<script>
+    (function(f,b){
+        var c;
+        f.hj=f.hj||function(){(f.hj.q=f.hj.q||[]).push(arguments)};
+        f._hjSettings={hjid:16007, hjsv:3};
+        c=b.createElement("script");c.async=1;
+        c.src="//static.hotjar.com/c/hotjar-16007.js?sv=3";
+        b.getElementsByTagName("head")[0].appendChild(c);
+    })(window,document);
+</script>
