23519722ce8c6e380acc29fec02fa1693ebf769b
Mike Bernat <mike.bernat@informulate.net>
2015-02-17 14:36:24 +0000

    Added published flag to user profile

--- app/views/layouts/partials/forms/profile.blade.php
+++ app/views/layouts/partials/forms/profile.blade.php
@@ -40,7 +40,10 @@
 		{{ Form::label('youtube', 'YouTube:') }}
 		{{ Form::text('youtube',is_object( $user->profile ) ? $user->profile->youtube : null, ['class' => 'form-control']) }}
 	</div>
-    {{ Form::hidden('published', true)  }}
+	<div class="form-group">
+		{{ Form::label('published', 'Allow others to find me in talent searches:') }}
+		{{ Form::checkbox('published', '1', is_object( $user->profile ) ? $user->profile->published : null) }}
+	</div>
 	<div class="form-group">
 		{{ Form::submit('Continue', ['id' => 'submit-profile','class' => 'btn btn-primary']) }}
 	</div>
