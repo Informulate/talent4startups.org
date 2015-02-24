b106e1d7354c65da4d8b6d428f61eff61c21785b
Jesus Fernandez <jesus.fndz@gmail.com>
2015-02-23 23:51:56 -0500

    Allow messages to be sent to anyone
    Signed-off-by: Jesus Fernandez <jesus.fndz@gmail.com>

--- app/controllers/MessagesController.php
+++ app/controllers/MessagesController.php
@@ -16,243 +16,245 @@ use Informulate\Users\UserRepository;
 class MessagesController extends BaseController
 {
 
-    private $userRepository;
+	private $userRepository;
 
-    /**
-     * Constructor
-     * @param UserRepository $userRepository
-     */
-    function __construct(UserRepository $userRepository)
-    {
-        $this->userRepository = $userRepository;
-        $this->beforeFilter('auth');
-    }
-    /**
-     * Show all of the message threads to the user
-     *
-     * @return mixed
-     */
-    public function index()
-    {
-        $currentUserId = Auth::user()->id;
-        $threads = Thread::ForUserByPriority($currentUserId)->paginate(10);
+	/**
+	 * Constructor
+	 * @param UserRepository $userRepository
+	 */
+	function __construct(UserRepository $userRepository)
+	{
+		$this->userRepository = $userRepository;
+		$this->beforeFilter('auth');
+	}
+	/**
+	 * Show all of the message threads to the user
+	 *
+	 * @return mixed
+	 */
+	public function index()
+	{
+		$currentUserId = Auth::user()->id;
+		$threads = Thread::ForUserByPriority($currentUserId)->paginate(10);
 
-        return View::make('messenger.index', compact('threads', 'currentUserId'));
-    }
+		return View::make('messenger.index', compact('threads', 'currentUserId'));
+	}
 
-    /**
-     * Shows a message thread
-     *
-     * @param $id
-     * @return mixed
-     */
-    public function show($id)
-    {
-        try {
-            $thread = Thread::findOrFail($id);
-        } catch (ModelNotFoundException $e) {
-            Session::flash('error_message', 'The thread with ID: ' . $id . ' was not found.');
+	/**
+	 * Shows a message thread
+	 *
+	 * @param $id
+	 * @return mixed
+	 */
+	public function show($id)
+	{
+		try {
+			$thread = Thread::findOrFail($id);
+		} catch (ModelNotFoundException $e) {
+			Session::flash('error_message', 'The thread with ID: ' . $id . ' was not found.');
 
-            return Redirect::to('messages');
-        }
+			return Redirect::to('messages');
+		}
 
-        // show current user in list if not a current participant
-        // $users = User::whereNotIn('id', $thread->participantsUserIds())->get();
+		// show current user in list if not a current participant
+		// $users = User::whereNotIn('id', $thread->participantsUserIds())->get();
 
-        // don't show the current user in list
-        $userId = Auth::user()->id;
-        $users = new ArrayObject(); //User::whereNotIn('id', $thread->participantsUserIds($userId))->get();
+		// don't show the current user in list
+		$userId = Auth::user()->id;
+		$users = new ArrayObject(); //User::whereNotIn('id', $thread->participantsUserIds($userId))->get();
 
-        $thread->markAsRead($userId);
-        $currentUserId = Auth::user()->id;
-        $messages = $thread->messages()->orderBy('created_at', 'desc')->paginate(5);
+		$thread->markAsRead($userId);
+		$currentUserId = Auth::user()->id;
+		$messages = $thread->messages()->orderBy('created_at', 'desc')->paginate(5);
 
-        return View::make('messenger.show', compact('thread', 'messages', 'users', 'currentUserId'));
-    }
+		return View::make('messenger.show', compact('thread', 'messages', 'users', 'currentUserId'));
+	}
 
-    /**
-     * Creates a new message thread
-     *
-     * @param null $recipient
-     * @return mixed
-     */
-    public function create($recipient = null)
-    {
-        $users = User::where('id', '!=', Auth::id())->get();
+	/**
+	 * Creates a new message thread
+	 *
+	 * @param null $recipient
+	 * @return mixed
+	 */
+	public function create($recipient = null)
+	{
+		$users = User::where('id', '!=', Auth::id())->get();
 
-        if (!empty($recipient)) {
-            $recipient = User::where('username', '=', $recipient)->first();
-        } else {
-            $recipient = new User();
-        }
+		if (!empty($recipient)) {
+			$recipient = User::where('username', '=', $recipient)->first();
+		} else {
+			$recipient = new User();
+		}
 
-        return View::make('messenger.create', compact('users', 'recipient'));
-    }
+		return View::make('messenger.create', compact('users', 'recipient'));
+	}
 
-    /**
-     * Stores a new message thread
-     *
-     * @return mixed
-     */
-    public function store()
-    {
-        $input = Input::all();
+	/**
+	 * Stores a new message thread
+	 *
+	 * @return mixed
+	 */
+	public function store()
+	{
+		$input = Input::all();
 
-        $thread = Thread::create(
-            [
-                'subject' => $input['subject'],
-            ]
-        );
+		$thread = Thread::create(
+			[
+				'subject' => $input['subject'],
+			]
+		);
 
-        // Message
-        Message::create(
-            [
-                'thread_id' => $thread->id,
-                'user_id'   => Auth::user()->id,
-                'body'      => strip_tags($input['message']),
-            ]
-        );
+		// Message
+		Message::create(
+			[
+				'thread_id' => $thread->id,
+				'user_id'   => Auth::user()->id,
+				'body'      => strip_tags($input['message']),
+			]
+		);
 
-        // Sender
-        Participant::create(
-            [
-                'thread_id' => $thread->id,
-                'user_id'   => Auth::user()->id,
-                'last_read' => new Carbon
-            ]
-        );
+		// Sender
+		Participant::create(
+			[
+				'thread_id' => $thread->id,
+				'user_id'   => Auth::user()->id,
+				'last_read' => new Carbon
+			]
+		);
 
-        // Recipients
-        if (Input::has('recipients')) {
-            $recipients = array_filter(explode(',', trim($input['recipients'])));
-            $recipientsAllowed = array();
+		// Recipients
+		if (Input::has('recipients')) {
+			$recipients = array_filter(explode(',', trim($input['recipients'])));
+			$recipientsAllowed = array();
 
-            foreach ($recipients as $recipient) {
-                if (UserRepository::canMessage($recipient)) {
-                    $recipientsAllowed[] = $recipient;
-                }
-            }
-            $thread->addParticipants($recipientsAllowed);
-        }
+			foreach ($recipients as $recipient) {
+				// Until further notice, remove the check to see if the participant is allowed to message each other
+//                if (UserRepository::canMessage($recipient)) {
+					$recipientsAllowed[] = $recipient;
+//                }
+			}
 
-        return Redirect::to('messages');
-    }
+			$thread->addParticipants($recipientsAllowed);
+		}
 
-    /**
-     * Adds a new message to a current thread
-     *
-     * @param $id
-     * @return mixed
-     */
-    public function update($id)
-    {
-        try {
-            $thread = Thread::findOrFail($id);
-        } catch (ModelNotFoundException $e) {
-            Session::flash('error_message', 'The thread with ID: ' . $id . ' was not found.');
+		return Redirect::to('messages');
+	}
 
-            return Redirect::to('messages');
-        }
+	/**
+	 * Adds a new message to a current thread
+	 *
+	 * @param $id
+	 * @return mixed
+	 */
+	public function update($id)
+	{
+		try {
+			$thread = Thread::findOrFail($id);
+		} catch (ModelNotFoundException $e) {
+			Session::flash('error_message', 'The thread with ID: ' . $id . ' was not found.');
 
-        $thread->activateAllParticipants();
+			return Redirect::to('messages');
+		}
 
-        // Message
-        Message::create(
-            [
-                'thread_id' => $thread->id,
-                'user_id'   => Auth::id(),
-                'body'      => strip_tags(Input::get('message')),
-            ]
-        );
+		$thread->activateAllParticipants();
 
-        // Add replier as a participant
-        $participant = Participant::firstOrCreate(
-            [
-                'thread_id' => $thread->id,
-                'user_id'   => Auth::user()->id
-            ]
-        );
-        $participant->last_read = new Carbon;
-        $participant->save();
+		// Message
+		Message::create(
+			[
+				'thread_id' => $thread->id,
+				'user_id'   => Auth::id(),
+				'body'      => strip_tags(Input::get('message')),
+			]
+		);
 
-        // Recipients
-        if (Input::has('recipients')) {
-            $thread->addParticipants(Input::get('recipients'));
-        }
+		// Add replier as a participant
+		$participant = Participant::firstOrCreate(
+			[
+				'thread_id' => $thread->id,
+				'user_id'   => Auth::user()->id
+			]
+		);
+		$participant->last_read = new Carbon;
+		$participant->save();
 
-        return Redirect::to('messages');
-    }
+		// Recipients
+		if (Input::has('recipients')) {
+			$thread->addParticipants(Input::get('recipients'));
+		}
 
-    /**
-     * Search for recipients when composing a message
-     * @param $query
-     * @return mixed
-     */
-    public function searchRecipients($query)
-    {
-        $users = $this->userRepository->search($query);
+		return Redirect::to('messages');
+	}
 
-        return Response::json($users);
-    }
+	/**
+	 * Search for recipients when composing a message
+	 * @param $query
+	 * @return mixed
+	 */
+	public function searchRecipients($query)
+	{
+		$users = $this->userRepository->search($query);
 
-    /**
-     * Mark thread read
-     *
-     * @param $threadId
-     * @return mixed
-     */
-    public function markRead($threadId)
-    {
-        try {
-            $thread = Thread::findOrFail($threadId);
-        } catch (ModelNotFoundException $e) {
-            Session::flash('error_message', 'The thread with ID: ' . $threadId . ' was not found.');
+		return Response::json($users);
+	}
 
-            return Redirect::to('messages');
-        }
+	/**
+	 * Mark thread read
+	 *
+	 * @param $threadId
+	 * @return mixed
+	 */
+	public function markRead($threadId)
+	{
+		try {
+			$thread = Thread::findOrFail($threadId);
+		} catch (ModelNotFoundException $e) {
+			Session::flash('error_message', 'The thread with ID: ' . $threadId . ' was not found.');
 
-        $thread->markAsRead(Auth::id());
+			return Redirect::to('messages');
+		}
 
-        return Redirect::to('messages/');
-    }
+		$thread->markAsRead(Auth::id());
 
-    /**
-     * Mark thread unread
-     *
-     * @param $threadId
-     * @return mixed
-     */
-    public function markUnread($threadId)
-    {
-        try {
-            $thread = Thread::findOrFail($threadId);
-        } catch (ModelNotFoundException $e) {
-            Session::flash('error_message', 'The thread with ID: ' . $threadId . ' was not found.');
+		return Redirect::to('messages/');
+	}
 
-            return Redirect::to('messages');
-        }
-        $thread->markAsUnRead(Auth::id());
+	/**
+	 * Mark thread unread
+	 *
+	 * @param $threadId
+	 * @return mixed
+	 */
+	public function markUnread($threadId)
+	{
+		try {
+			$thread = Thread::findOrFail($threadId);
+		} catch (ModelNotFoundException $e) {
+			Session::flash('error_message', 'The thread with ID: ' . $threadId . ' was not found.');
 
-        return Redirect::to('messages/');
-    }
+			return Redirect::to('messages');
+		}
+		$thread->markAsUnRead(Auth::id());
 
-    /**
-     * Delete a thread
-     *
-     * @param $threadId
-     * @return mixed
-     */
-    public function delete($threadId)
-    {
-        try {
-            $thread = Thread::findOrFail($threadId);
-        } catch (ModelNotFoundException $e) {
-            Session::flash('error_message', 'The thread with ID: ' . $threadId . ' was not found.');
+		return Redirect::to('messages/');
+	}
 
-            return Redirect::to('messages');
-        }
-        $thread->delete();
+	/**
+	 * Delete a thread
+	 *
+	 * @param $threadId
+	 * @return mixed
+	 */
+	public function delete($threadId)
+	{
+		try {
+			$thread = Thread::findOrFail($threadId);
+		} catch (ModelNotFoundException $e) {
+			Session::flash('error_message', 'The thread with ID: ' . $threadId . ' was not found.');
 
-        return Redirect::to('messages/');
-    }
+			return Redirect::to('messages');
+		}
+		$thread->delete();
+
+		return Redirect::to('messages/');
+	}
 }
