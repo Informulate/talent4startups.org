5cc65c07ba7eaa0dc9afde50b983b84755e4f2dc
Frank Bennett <frank.bennett@informulate.net>
2015-02-28 13:43:32 -0500

    add cache to twitter feeds dev Complete: #140

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -18,10 +18,25 @@ class BaseController extends Controller {
 			'currentUser' 		=> Auth::user(),
 			'facebookFeed'		=> FeedReader::read(Config::get('feeds.facebookFeed'))->get_items(),
 			't4sBlogFeed' 		=> FeedReader::read(Config::get('feeds.blogFeed'))->get_items(),
-			'twitterFeed'	 	=> Twitter::getUserTimeline(array('screen_name' => Config::get('feeds.twitterScreenName'), 'count' => 3, 'format' => 'object')),
-			'twitterHomeFeed' 	=> Twitter::getHomeTimeline(array('screen_name' => Config::get('feeds.twitterScreenName'), 'count' => 3, 'format' => 'object')),
+			'twitterFeed'	 	=> $this->getTwitterFeed('twitterFeed'),
+			'twitterHomeFeed' 	=> $this->getTwitterFeed('twitterHomeFeed'),
 			'displayAds' 		=> getenv('DISPLAY_ADS')
 		]);
 	}
 
+	protected function getTwitterFeed($feedname) {
+
+		if (!Cache::has($feedname)) {
+
+			if (strpos($feedname, 'Home')) {
+				$timeline = Twitter::getHomeTimeline(array('screen_name' => Config::get('feeds.twitterScreenName'), 'count' => 3, 'format' => 'object'));
+			} else {
+				$timeline = Twitter::getUserTimeline(array('screen_name' => Config::get('feeds.twitterScreenName'), 'count' => 3, 'format' => 'object'));
+			}
+
+			Cache::add($feedname, $timeline, 60);
+		}
+
+		return Cache::get($feedname);
+	}
 }
