30875e0ccea548c56033563849f7747da140426f
Mike Bernat <mike.bernat@informulate.net>
2015-05-20 18:00:17 +0000

    Removed escaping from email templates

--- resources/views/messenger/templates/startup/join/invite/talent.blade.php
+++ resources/views/messenger/templates/startup/join/invite/talent.blade.php
@@ -5,7 +5,7 @@ You have been invited to be a part of {{ $startup->name }}.
 
 @section('body')
 Hi {{ $recipient->profile->first_name }},
-Good job, you have been invited to apply to {{ link_to_route('startup_profile', $startup->name, $startup->url) }}. If you like what you see, apply early to beat out other applicants.
+Good job, you have been invited to apply to {!! link_to_route('startup_profile', $startup->name, $startup->url) !!}. If you like what you see, apply early to beat out other applicants.
 
 Remember to come prepared to talk about how your skills can apply to the project's specific needs. Hope you get it!
 @endsection
--- resources/views/messenger/templates/startup/left/founder.blade.php
+++ resources/views/messenger/templates/startup/left/founder.blade.php
@@ -6,7 +6,7 @@
 @section('body')
 Hi {{ $recipient->profile->first_name }},
 
-FYI, {{ link_to_route('profile_path', $talent->profile->first_name . ' ' . $talent->profile->last_name, $talent->id) }} has left your project: {{ link_to_route('startup_profile', $startup->name, $startup->url) }}. All things come to an end. :(
+FYI, {!! link_to_route('profile_path', $talent->profile->first_name . ' ' . $talent->profile->last_name, $talent->id) !!} has left your project: {!! link_to_route('startup_profile', $startup->name, $startup->url) !!}. All things come to an end. :(
 
 Ratings are a crucial part of T4S, and helps to maintain a vibrant community. Please take a minute to rate {{ $recipient->profile->first_name }}'s performance.
 @endsection
