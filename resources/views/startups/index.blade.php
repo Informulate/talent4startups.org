95f3670995765f370576b9195cb96b22944e7174
Mike Bernat <mike.bernat@informulate.net>
2015-08-12 13:16:11 -0400

    Ticket #238 - Talent and Startup Search Tweak

--- resources/views/startups/index.blade.php
+++ resources/views/startups/index.blade.php
@@ -5,7 +5,7 @@
 	<div class="row"> <!-- display search fields -->
 		{!! Form::open(['route' => 'startups.index','name'=>'project-search-form','id'=>'project-search-form', 'method' => 'get']) !!}
 			<div class="form-group col-sm-3">
-				{!! Form::select('needs', array('Startups that Need: Everyone') + $needs, Input::get('needs'),['id'=>'needs','class' => 'form-control']); !!}
+				{!! Form::select('needs', array('Browse: All Roles') + $needs, Input::get('needs'),['id'=>'needs','class' => 'form-control']); !!}
 			</div>
 			<div class="form-group col-sm-3">
 				{!! Form::text('tag', Input::get('tag'), ['id'=>'tag','class' => 'form-control', 'placeholder' => 'Enter tag']) !!}
