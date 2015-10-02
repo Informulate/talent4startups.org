a3bb23437468d5ebd6b921e804169840d8a48f6e
Mike Bernat <mike.bernat@informulate.net>
2015-10-02 16:11:38 -0400

    Attempt to remove foreign char in email template

--- resources/views/messenger/templates/startup/join/request/owner.blade.php
+++ resources/views/messenger/templates/startup/join/request/owner.blade.php
@@ -1,11 +1,11 @@
 @extends('messenger.templates.base')
 @section('subject')
-{{ $talent->first_name }} {{ $talent->last_name }} has applied to join {{ $startup->name }}.
+    {{ $talent->first_name }} {{ $talent->last_name }} has applied to join {{ $startup->name }}.
 @endsection
 
 @section('body')
-{!! link_to_route('profile_path', $talent->first_name . ' ' . $talent->last_name, $talent->id) !!} has applied to join {!! link_to_route('startup_profile', $startup->name, $startup->url) !!}.
+    {!! link_to_route('profile_path', $talent->first_name . ' ' . $talent->last_name, $talent->id) !!} has applied to join {!! link_to_route('startup_profile', $startup->name, $startup->url) !!}.
 
 
-{!! link_to_route('startup_profile', 'Click here to approve or reject', $startup->url) !!}
+    {!! link_to_route('startup_profile', 'Click here to approve or reject', $startup->url) !!}
 @endsection
