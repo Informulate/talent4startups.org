03cfbd9c6508af38d82772efa67664a1711d6e17
Mike Bernat <mike.bernat@informulate.net>
2015-10-03 22:12:46 +0000

    oix landing page redesign v 2.1

--- resources/views/communities/join2.blade.php
+++ resources/views/communities/join2.blade.php
@@ -22,15 +22,15 @@
 					<div class="container">
 						<div id="community-join">
 							<div class="col-lg-12 text-center">
-								<img src="/images/logo-white.png" alt="T4S White Logo" width="65%" />
-								<h4 style="font-size: 1.7em; font-weight: bold; margin-bottom: 30px;">T4S.US builds local communities that connects audacious startups to brilliant talent</h4>
-								<h4 style="font-weight: bold; margin-bottom: 25px;">Talent4Startups is proud to welcome OiX attendees</h4>
-								<h2 style="margin-top: 0; margin-bottom: 30px; font-size: 3.3em">Sign up is free and easy</h2>
+								<img src="/images/logo-white.png" alt="T4S White Logo" width="45%" style="margin: 20px 0;"/>
+								<h4 style="font-size: 2.2em; font-weight: bold; margin-bottom: 23px;">We connect audacious startups to brilliant talent</h4>
+								<hr width="55%"/>
+								<h4 style="font-weight: bold; margin-bottom: 25px;">Talent4Startups is proud to welcome OiX attendees to our community!</h4>
 								<div class="text-center">
 									<div style="margin-bottom: 20px;">
 										<a id="sign-in-linked_in" href="{{ route("linked_in", ['join' => $community->url]) }}" alt="Sign Up with LinkedIn"><img src="{{ asset('images/signin-linkedin.png') }}" alt="" /></a>
 									</div>
-									<p class="" style="font-weight: normal; font-size: 1.5em;"><a id="email-form-link" href="#">Or Signup with email</a></p>
+									<p class="" style="font-weight: normal; font-size: 1.5em; "><a id="email-form-link" href="#" style="text-decoration: none">Or Signup with email</a></p>
 									<p class="text-center">Already have an account? <a href="{{ route('community.login', ['url' => $community->url]) }}">Log in</a></p>
 									<div class="clearfix"></div>
 									<form id="email-form" class="form" style="display: none;" role="form" method="POST" action="{{ url('/auth/register') }}">
@@ -41,7 +41,8 @@
 									</form>
 								</div>
 								<img class="community-logo" src="/images/oix/OiX_mark_countdown2.png" alt="OiX mark countdown2">
-								<p>OiX attendees registering during the event October 2-6th will be automatically entered in a drawing to win one of our fabulous T4S T-Shirts!</p>
+								<div class="clearfix"></div>
+								<p style="" class="col-lg-6 col-md-offset-3">OiX attendees registering during the event October 2-6, 2015 will be automatically entered in a drawing to win one of our fabulous T4S T-Shirts!</p>
 							</div>
 						</div>
 					</div>
