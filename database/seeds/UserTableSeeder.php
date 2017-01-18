8aafc0a15052e51908e352131863d3e6847ed9b7
Jesus Fernandez <jesus.fndz@gmail.com>
2017-01-18 11:53:59 -0500

    Prevent real emails from been used by faker
    Signed-off-by: Jesus Fernandez <jesus.fndz@gmail.com>

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -49,7 +49,11 @@ class UserTableSeeder extends Seeder
 		$skills = Skill::all();
 
 		for ($i = 0; $i < 50; $i++) {
-			$user = User::register($faker->firstName, $faker->lastName, $faker->unique()->userName, $faker->unique()->email, bcrypt('password'), 'talent');
+		    // Faker generates real email accounts so to prevent mail from delivering to real email accounts just use t4s.dev as the email.
+		    $username = $faker->unique()->userName;
+		    $email = $username . "@t4s.dev";
+
+			$user = User::register($faker->firstName, $faker->lastName, $username, $email, bcrypt('password'), 'talent');
 			$this->userRepository->save($user);
 
 			$profileData = [
